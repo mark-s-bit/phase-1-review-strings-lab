6b4d19f0c393021a2e62b9fe6babffcbc2cfb464
mark-s-bit <markchege03@gmail.com>
2024-07-11 08:53:38 +0300

    intial

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'light Yagami';
+
+const welcomeMessage =`Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting =  `Welcome, to ${currentUser.slice(0,1)}!`
